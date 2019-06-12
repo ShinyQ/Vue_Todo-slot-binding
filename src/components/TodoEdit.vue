2ca83803963a5f806608210a07137d1dd1cc0ac8
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2019-06-13 06:15:07 +0700

    CRUD Todo

---
+++ src/components/TodoEdit.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="edit">
+        <input type="text" v-model="dataEdit">            
+        <button @click="$emit('ubahTodo',dataEdit)">Ubah Todo</button>
+        <button @click="$emit('cancelEdit')">Batal</button>        
+    </div>
+</template>
+<script>
+export default {
+    name:"TodoEdit",
+    props:{
+        editValue:String,
+        editId:Number,
+    },
+    data() {
+        return {
+            dataEdit:''
+        }
+    },   
+    mounted() {
+      this.dataEdit=this.editValue  
+    },
+}
+</script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="item">
+        <input :checked="todo.status" type="checkbox" @click="$emit('checkTodo')">
+        <span :class="{ checked: todo.status }">
+            {{numberList}}. {{todo.name}}
+        </span>
+        <button :disabled="todo.status" @click="$emit('editTodo')">Edit Todo</button>
+        <button @click="$emit('removeTodo')">Hapus Todo</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TodoItem',
+    props:{
+        todo: Object,
+        index: Number,        
+    },
+    computed: {
+        numberList:function(){
+            return this.index+1
+        }
+    },
+}
+</script>
